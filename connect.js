a1155f2d39836334dd74ae210675e6c84028dd11
eshhacb <eshhacb@gmail.com>
2024-12-13 14:56:22 +0530

    Add files via upload

---
+++ connect.js
@@ -0,0 +1,11 @@
+//for connecting mongoose
+
+const mongoose=require('mongoose');
+
+async function connectToMongoDB(url){
+    return mongoose.connect(url);
+}
+
+module.exports={
+    connectToMongoDB,
+};
---
+++ routes/url.js
@@ -0,0 +1,9 @@
+const express= require("express");
+const {handleGenerateNewShortURL,handleGetAnalytics}=require('../controllers/url');
+const { model } = require("mongoose");
+const router=express.Router();
+
+router.post('/',handleGenerateNewShortURL);
+router.get('/analytics/:shortId',)
+
+module.exports=router;
