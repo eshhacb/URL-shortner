d3e66dd6eeff2d27145fead780249d0dc809ae19
Esha Raj <eshhacb@gmail.com>
2024-12-14 15:56:27 +0530

    Nanoid updated and the version is changed

--- controllers/url.js
+++ controllers/url.js
@@ -1,10 +1,16 @@
-const shortID=require('shortid');
+let nanoid;
+(async()=>{
+    const module=await import
+    ('nanoid');
+    nanoid=module.nanoid;
+})();
+
 const URL=require('../models/url');
 
 async function handleGenerateNewShortURL(req,res){
     const body=req.body;
     if(!body.url) return res.status(400).json({error: "url is required"})
-    const shortID=shortid();
+    const shortID=nanoid();
 
     await URL.create({
         shortId:shortID,
@@ -16,7 +22,7 @@ async function handleGenerateNewShortURL(req,res){
 
 async function handleGetAnalytics(req,res){
     const shortId=req.params.shortId;
-    await URL.findOne({shortId});
+    const result = await URL.findOne({ shortId });
     return res.json({totalClicks:result.visitHistory.length, analytics:result.visitHistory});
 }
 
